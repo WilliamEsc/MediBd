5e78fc78eba1302b8be6b690e54750577cdf2172
ESCRIVA WILLIAM p1702268 <william.escriva@etu.univ-lyon1.fr>
2020-12-27 10:30:12 +0100

    ajout du server a la racine

---
+++ server-medidb/models/scheme.js
@@ -0,0 +1,109 @@
+const mongoose = require('mongoose');
+const schema = mongoose.Schema;
+const objectId = mongoose.ObjectId;
+
+//schema.Types.String.set('trim', true);
+
+const specialiteSchema = schema({
+    id: objectId,
+    codeCis:Number,
+    denomination: String,
+    formePharmaceutique:String,
+    voiesAdministration: String,
+    statutAdministratif: String,
+    typeProcedureAMM: String,
+    etatDeCommercialisation: String,
+    dateAMM: String,
+    statutBdm: String,
+    numeroAutorisationEuropeenne: String,
+    titulaire: String,
+    surveillanceRenforcee: String
+});
+
+const presentationSchema = schema({
+    id: objectId,
+    codeCis: String,
+    codeCip7: String,
+    libelle: String,
+    statusAdmninistratif: String,
+    etatCommercialisation: String,
+    dateDeclaration: String,
+    codeCIP13: String,
+    agrementCollectivites: String,
+    tauxRemboursement: String,
+    prix: String,
+    indications: String
+});
+
+const compositionSchema = schema({
+    id: objectId,
+    codeCis: String,
+    designationElementPharmaceutique: String,
+    codeSubstance: String,
+    denominationSubstance: String,
+    dosageSubstance: String,
+    referenceDosage: String,
+    natureComposant: String,
+    lienEntreSubstancesActivesEtFractionsTherapeutiques: String
+});
+
+const avisSMRSchema = schema({
+    id: objectId,
+    codeCis: String,
+    codeDossierHAS: String,
+    motifEvaluation: String,
+    dateAvisCommissionTransparence: String,
+    valeurSMR: String,
+    libelleSMR: String
+});
+
+const avisASMRSchema = schema({
+    id: objectId,
+    codeCis: String,
+    codeDossierHAS: String,
+    motifEvaluation: String,
+    dateAvisCommissionTransparence: String,
+    valeurASMR: String,
+    libelleASMR: String
+});
+
+const avisCTSchema = schema({
+    id: objectId,
+    CodeDossierHAS: String,
+    lienVersPagesAvis: String
+});
+
+const conditionPrescDelivSchema = schema({
+    id: objectId,
+    codeCis: String,
+    conditionPrescriptionOuDelivrance: String
+});
+
+const infosImportantesSchema = schema({
+    id: objectId,
+    codeCis: String,
+    dateDebutInformationSecurite: String,
+    dateFinInformationSecurite: String,
+    texteAAfficherEtLienInfoSecurite: String
+});
+
+
+const specialite = mongoose.model("specialite", specialiteSchema);
+const presentation = mongoose.model("presentation", presentationSchema);
+const composition = mongoose.model("composition", compositionSchema);
+const avisSMR = mongoose.model("avis_SMR_de_la_HAS", avisSMRSchema);
+const avisASMR = mongoose.model("avis_ASMR_de_la_HAS", avisASMRSchema);
+const avisCT = mongoose.model("avis_CT_de_la_HAS", avisCTSchema);
+const conditionPrescDeliv = mongoose.model("condition_prescription_et_delivrance", conditionPrescDelivSchema);
+const infosImportantes = mongoose.model("infos_importantes", infosImportantesSchema);
+
+module.exports = {
+    specialite: specialite,
+    presentation: presentation,
+    composition: composition,
+    avisSMR: avisSMR,
+    avisASMR: avisASMR,
+    avisCT: avisCT,
+    conditionPrescDeliv: conditionPrescDeliv,
+    infosImportantes: infosImportantes
+}
---
+++ server-medidb/routes/api.js
@@ -0,0 +1,259 @@
+var express = require('express');
+var router = express.Router();
+
+const mongoose = require("mongoose");
+mongoose.connect("mongodb://localhost/test", { useNewUrlParser: true });
+const { specialite, composition, presentation, avisASMR, avisCT, avisSMR, infosImportantes, conditionPrescDeliv } = require("../models/scheme");
+const groupeGenerique = require("../models/groupeGenerique");
+const proportionPrincep = require("../models/proportionPrinceps");
+
+const db = mongoose.connection;
+db.on('error', console.error.bind(console, 'connection error:'));
+db.once('open', async function () {
+    /*
+    db.db.listCollections().toArray(function (err, names) {
+        if (err) {
+            console.log(err);
+        } else {
+            names.map((name) => {
+                if (name.name == "proportionPrincep") {
+                    mongoose.connection.db.dropCollection(
+                        "proportionPrincep",
+                        function (err, result) {
+                            console.log("Collection droped");
+                        }
+                    );
+                }
+            })
+        }
+    });
+
+    const data = await specialite.aggregate([
+        {
+            $lookup: {
+                from: groupeGenerique.collection.name,
+                localField: "codeCis",
+                foreignField: "codeCis",
+                as: "gene"
+            }
+        },
+        {
+            $unwind: {
+                "path": "$gene",
+                "preserveNullAndEmptyArrays": true
+            }
+        }
+    ])
+        .group({
+            _id: { titulaire: "$titulaire" },
+            nb_med: { $sum: 1 },
+            nb_princ: { $sum: { "$cond": [{ "$eq": ["$gene.typeGenerique", 0] }, 1, 0] } }
+        })
+        .exec()
+        .then(docs => {
+            let copy = [];
+
+            docs.map(element => {
+                copy.push({ titulaire: element._id.titulaire.trim(), nb_med: element.nb_med, nb_princ: element.nb_princ });
+            })
+
+            return copy;
+        });
+
+
+
+    db.createCollection("proportionPrincep", (err, result) => {
+        if (err) throw err;
+        console.log("proportionPrincep created");
+
+        db.collection("proportionPrincep").insertMany(data, (err, result) => {
+            if (err) throw err;
+            console.log("donnees insere");
+        });
+    });*/
+
+    console.log("connection ready");
+});
+
+router.get('/', function (req, res, next) {
+    res.send(200, ({ sucess: true }));
+});
+
+router.get('/getTitulaire', function (req, res, next) {
+    specialite.distinct("titulaire").exec(function (err, data) {
+        if (err) throw err;
+        if (data.length === 0) {
+            res.status(404).send('no data found');
+            return;
+        }
+        res.send({ value: data });
+    });
+});
+
+router.post('/getPropPrincLab', function (req, res, next) {
+    specialite.aggregate([
+        { $match: { titulaire: " " + req.body.titulaire } },
+        {
+            $lookup: {
+                from: groupeGenerique.collection.name,
+                localField: "codeCis",
+                foreignField: "codeCis",
+                as: "gene"
+            }
+        },
+        {
+            $unwind: {
+                "path": "$gene",
+                "preserveNullAndEmptyArrays": true
+            }
+        }
+    ])
+        .group({
+            _id: { titulaire: "$titulaire" },
+            nb_med: { $sum: 1 },
+            nb_princ: { $sum: { "$cond": [{ "$eq": ["$gene.typeGenerique", 0] }, 1, 0] } }
+        })
+        .exec()
+        .then(docs => {
+            let copy = [];
+
+            docs.map(element => {
+                copy.push({ titulaire: element._id.titulaire.trim(), nb_med: element.nb_med, nb_princ: element.nb_princ });
+            })
+            console.log(docs);
+            res.send({ value: copy });
+        });
+});
+
+router.get('/getTest', function (req, res, next) {
+    specialite.aggregate([
+        { $match: { titulaire: " " + "BOIRON" } },
+        {
+            $lookup: {
+                from: groupeGenerique.collection.name,
+                localField: "codeCis",
+                foreignField: "codeCis",
+                as: "gene"
+            }
+        },
+        { $project: { dateAMM: 1, "gene.typeGenerique": 1 } },
+        {
+            $unwind: {
+                "path": "$gene",
+                "preserveNullAndEmptyArrays": true
+            }
+        }
+    ])
+        .exec()
+        .then(docs => {
+            let ret = [];
+            docs.map(element => {
+                let date = element.dateAMM.split('/');
+                if (!ret.some(e => e.annee === date[2])) {
+                    if (element.gene && element.gene.typeGenerique == 0) {
+                        ret.push({ annee: date[2], princep: 1, generique: 0 });
+                    } else {
+                        ret.push({ annee: date[2], princep: 0, generique: 1 });
+                    }
+                } else {
+                    ret.some(e => {
+                        if (e.annee === date[2]) {
+                            if (element.gene && element.gene.typeGenerique == 0) {
+                                e.princep++;
+                            } else {
+                                e.generique++;
+                            }
+                        }
+                    }
+                    );
+                }
+            });
+            ret.sort((a,b) => (a.annee > b.annee) ? 1 : ((b.annee > a.annee) ? -1 : 0)); 
+
+            res.send({ value: ret });
+        });
+});
+
+/*router.post('/getPropPrincLab', function (req, res, next) {
+    proportionPrincep.find({ titulaire: req.body.titulaire }, function (err, data) {
+        if (err) throw err;
+        if (data.length === 0) {
+            res.status(404).send('no data found');
+            return;
+        }
+        res.send({ value: data });
+    });
+});*/
+
+router.post('/getMedi', function (req, res, next) {
+    const pageOptions = {
+        page: parseInt(req.body.page, 10) || 0,
+        limit: parseInt(req.body.limit, 10) || 100
+    }
+    Object.entries(req.body).forEach(([key, value]) => {
+        console.log(key + ' : ' + value);
+    });
+    let recherche = {};
+    if (req.body.research) {
+        recherche = { ...recherche, denomination: { "$regex": req.body.research, "$options": 'i' } };
+    }
+    if (req.body.titulaire) {
+        recherche = { ...recherche, titulaire: ' ' + req.body.titulaire };
+    }
+    console.log(recherche);
+    specialite.find(recherche, function (err, data) {
+        if (err) throw err;
+        if (data.length === 0) {
+            res.status(404).send('no data found');
+            return;
+        }
+        res.send({ value: data });
+    }).limit(pageOptions.limit).skip(pageOptions.limit * pageOptions.page);
+});
+
+router.get('/getAllMedi', function (req, res, next) {
+    specialite.find({}, { denomination: 1 }, function (err, data) {
+        if (err) throw err;
+        if (data.length === 0) {
+            res.status(404).send('no data found');
+            return;
+        }
+        res.send({ value: data });
+    }).sort({ denomination: 1 });
+});
+
+router.get('/getForme', async function (req, res, next) {
+    const data = await specialite.aggregate([
+        { $group: { _id: { formePharmaceutique: "$formePharmaceutique" } } },
+        { $sort: { '_id.formePharmaceutique': 1 } }
+    ]);
+    res.send({ value: data.map(entry => { return entry._id.formePharmaceutique }) });
+});
+
+router.get('/getVoies', async function (req, res, next) {
+    const data = await specialite.find({}).select({ voiesAdministration: 1 });
+    const tab = data.map(entry => { return entry.voiesAdministration });
+    let tabReturn = [];
+    tab.map(entry => {
+        const voie = entry.split(';');
+        voie.map(_voie => {
+            if (!tabReturn.includes(_voie)) {
+                tabReturn.push(_voie);
+            }
+        });
+    });
+    tabReturn.sort();
+    //console.log(tabReturn);
+    res.send({ value: tabReturn.map(entry => { return entry }) });
+});
+
+router.get('/getAMM', async function (req, res, next) {
+
+    const data = await specialite.aggregate([
+        { $group: { _id: { typeProcedureAMM: "$typeProcedureAMM" } } },
+        { $sort: { '_id.typeProcedureAMM': 1 } }
+    ]);
+    res.send({ value: data.map(entry => { return entry._id.typeProcedureAMM }) });
+});
+
+module.exports = router;
